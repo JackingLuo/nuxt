b48eda049a5b41a4c5434eb19126ae65c7a3e2d2
Arbort <781642016@qq.com>
2019-09-19 19:20:41 +0800

    上传图片

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="body">
     <p>首页</p>
+    <input type="file" @change="changeImg" ref="inputer">
   </div>
 </template>
 
 <script>
+  import axios from "axios";
   export default {
     layout: 'myLayout',
     head() {
@@ -20,6 +22,27 @@
       }
     },
     methods:{
+      afterRead(file) {
+        // 此时可以自行将文件上传至服务器
+        console.log(file);
+      },
+      changeImg(){
+        /*
+        * 以下注释为上传图片
+        * */
+        let inputDOM = this.$refs.inputer;
+        let file =inputDOM.files[0];//上传的文件
+        let param = new FormData(); //创建form对象
+        param.append('file',file);//通过append向form对象添加数据
+        let config = {
+          headers:{'Content-Type':'multipart/form-data'}
+        }; //添加请求头
+        axios.post('http://localhost:8099/upload/image',param,config)
+          .then(response=>{
+            console.log(response.data);
+        })
+
+      },
       addLi(value){
         this.inputList.push({
           value:''
@@ -33,7 +56,11 @@
 </script>
 
 <style scoped>
-
+  .someStyle{
+    width: 50px;
+    height: 50px;
+    border: 1px solid red;
+  }
 </style>
 
 
--- plugins/iview.js
+++ plugins/iview.js
@@ -1,3 +1,4 @@
 import Vue from "vue";
-import {Icon,Dialog,Field,CellGroup, Button,Notify,Swipe,SwipeItem,Lazyload,Toast}  from "vant";
-Vue.use(Icon).use(Dialog).use(Field).use(CellGroup).use(Button).use(Notify).use(Swipe).use(SwipeItem).use(Lazyload).use(Toast);
+import 'vant/lib/index.css';
+import {Icon,Dialog,Field,CellGroup, Button,Notify,Swipe,SwipeItem,Lazyload,Toast,Uploader}  from "vant";
+Vue.use(Icon).use(Dialog).use(Field).use(CellGroup).use(Button).use(Notify).use(Swipe).use(SwipeItem).use(Lazyload).use(Toast).use(Uploader);
